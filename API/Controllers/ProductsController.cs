990a92db2138381421bca4e1c0d9d8989009a0e9
Brad Trehan <bradt@vogcalgaryappdeveloper.com>
2021-03-13 22:07:07 -0700

    added error handling and swagger suppourt

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -2,20 +2,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using API.DTOs;
+using API.Errors;
 using AutoMapper;
 using Core.Entities;
 using Core.Interfaces;
 using Core.Specifications;
 using Infrastructure.Data;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
-    [ApiController]
-    [Route("api/[controller]")]
 
-    public class ProductsController : ControllerBase
+    public class ProductsController : BaseApiController
     {
 
         // // This is by using the non -repo pattern method
@@ -80,6 +80,7 @@ namespace API.Controllers
 
 
         [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         public async Task<ActionResult<ProductToReturnDto>> GetProducts(int id)
         {
             // Non- Repo pattern implementation 
@@ -95,6 +96,8 @@ namespace API.Controllers
 
             //Adding 
             var products = await _productRepo.GetEntityWithSpec(spec);
+
+            if (products == null) return NotFound((new ApiResponse(404)));
 
             // without Auto Mapper
             // return new ProductToReturnDto
